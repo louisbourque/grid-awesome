4dc483c514a51bb20483326759d0bc7e154c27f2
Louis Bourque <louis@louisbourque.ca>
2017-09-04 23:04:12 -0600

    Always show output css

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,85 +1,87 @@
 <template>
 	<div class="editor">
-    <div class="input">
-      <p class="center">Grid Template Areas: {{areas.length}}</p>
-      <button class="input__button input__button--add" @click="addArea">Add an area</button>
-    </div>
-    <div class="input">
-      <p class="center">Grid Template Columns: {{columns.length}}</p>
-      <div v-for="column, index in columns">
-        Column {{index+1}} 
-        <input v-model="column.size" class="input__input--size" type="number" name="size" size="5" :disabled="column.units === 'auto'">
-        <select v-model="column.units">
-          <option val="auto">auto</option>
-          <option val="%">%</option>
-          <option val="em">em</option>
-          <option val="rem">rem</option>
-          <option val="fr">fr</option>
-          <option val="px">px</option>
-          <option val="vw">vw</option>
-        </select>
-        <button class="input__button input__button--remove" v-if='columns.length > 1' @click="removeColumn(column)">X</button>
+    <div class="inputs">
+      <div class="input">
+        <p class="center">Grid Template Areas: {{areas.length}}</p>
+        <button class="input__button input__button--add" @click="addArea">Add an area</button>
       </div>
-      <button class="input__button input__button--add" @click="addColumn">Add Column</button>Remove Column</button>
-    </div>
-    <div class="input">
-      <p class="center">Grid Template Rows: {{rows.length}}</p>
-      <div v-for="row, index in rows">
-        Row {{index+1}} 
-        <input v-model="row.size" class="input__input--size" type="number" name="size" size="5" :disabled="row.units === 'auto'">
-        <select v-model="row.units">
-          <option val="auto">auto</option>
-          <option val="%">%</option>
-          <option val="em">em</option>
-          <option val="rem">rem</option>
-          <option val="fr">fr</option>
-          <option val="px">px</option>
-          <option val="vh">vh</option>
-        </select>
-        <button class="input__button input__button--remove" v-if='rows.length > 1' @click="removeRow(row)">X</button>
+      <div class="input">
+        <p class="center">Grid Template Columns: {{columns.length}}</p>
+        <div v-for="column, index in columns">
+          Column {{index+1}} 
+          <input v-model="column.size" class="input__input--size" type="number" name="size" size="5" :disabled="column.units === 'auto'">
+          <select v-model="column.units">
+            <option val="auto">auto</option>
+            <option val="%">%</option>
+            <option val="em">em</option>
+            <option val="rem">rem</option>
+            <option val="fr">fr</option>
+            <option val="px">px</option>
+            <option val="vw">vw</option>
+          </select>
+          <button class="input__button input__button--remove" v-if='columns.length > 1' @click="removeColumn(column)">X</button>
+        </div>
+        <button class="input__button input__button--add" @click="addColumn">Add Column</button>Remove Column</button>
       </div>
-      <button class="input__button input__button--add" @click="addRow">Add Row</button>
-      
-    </div>
-    <div class="input">
-      <p class="center">Grid Column Gap</p>
-      <input v-model="colGap.size" class="input__input--size" type="number" name="size" size="5">
-        <select v-model="colGap.units">
-          <option val="%">%</option>
-          <option val="em">em</option>
-          <option val="rem">rem</option>
-          <option val="px">px</option>
-        </select>
-    </div>
-    <div class="input">
-      <p class="center">Grid Row Gap</p>
-      <input v-model="rowGap.size" class="input__input--size" type="number" name="size" size="5">
-        <select v-model="rowGap.units">
-          <option val="%">%</option>
-          <option val="em">em</option>
-          <option val="rem">rem</option>
-          <option val="px">px</option>
-        </select>
-    </div>
-    <div class="input">
-      <p class="center">Justify Items</p>
-        <select v-model="justify.value">
-          <option val=""></option>
-          <option val="stretch">stretch</option>
-          <option val="center">center</option>
-          <option val="start">start</option>
-          <option val="end">end</option>
-        </select>
-    </div>
-    <div class="input">
-      <p class="center">Align Items</p>
-        <select v-model="align.value">
-          <option val=""></option>
-          <option val="stretch">stretch</option>
-          <option val="center">center</option>
-          <option val="start">start</option>
-          <option val="end">end</option>
-        </select>
+      <div class="input">
+        <p class="center">Grid Template Rows: {{rows.length}}</p>
+        <div v-for="row, index in rows">
+          Row {{index+1}} 
+          <input v-model="row.size" class="input__input--size" type="number" name="size" size="5" :disabled="row.units === 'auto'">
+          <select v-model="row.units">
+            <option val="auto">auto</option>
+            <option val="%">%</option>
+            <option val="em">em</option>
+            <option val="rem">rem</option>
+            <option val="fr">fr</option>
+            <option val="px">px</option>
+            <option val="vh">vh</option>
+          </select>
+          <button class="input__button input__button--remove" v-if='rows.length > 1' @click="removeRow(row)">X</button>
+        </div>
+        <button class="input__button input__button--add" @click="addRow">Add Row</button>
+        
+      </div>
+      <div class="input">
+        <p class="center">Grid Column Gap</p>
+        <input v-model="colGap.size" class="input__input--size" type="number" name="size" size="5">
+          <select v-model="colGap.units">
+            <option val="%">%</option>
+            <option val="em">em</option>
+            <option val="rem">rem</option>
+            <option val="px">px</option>
+          </select>
+      </div>
+      <div class="input">
+        <p class="center">Grid Row Gap</p>
+        <input v-model="rowGap.size" class="input__input--size" type="number" name="size" size="5">
+          <select v-model="rowGap.units">
+            <option val="%">%</option>
+            <option val="em">em</option>
+            <option val="rem">rem</option>
+            <option val="px">px</option>
+          </select>
+      </div>
+      <div class="input">
+        <p class="center">Justify Items</p>
+          <select v-model="justify.value">
+            <option val=""></option>
+            <option val="stretch">stretch</option>
+            <option val="center">center</option>
+            <option val="start">start</option>
+            <option val="end">end</option>
+          </select>
+      </div>
+      <div class="input">
+        <p class="center">Align Items</p>
+          <select v-model="align.value">
+            <option val=""></option>
+            <option val="stretch">stretch</option>
+            <option val="center">center</option>
+            <option val="start">start</option>
+            <option val="end">end</option>
+          </select>
+      </div>
     </div>
     <div class="css-output">
     <button class="input__button button--clipboard" @click="copyTextToClipboard(css)">Copy to Clipboard</button>
@@ -122,13 +124,27 @@ export default {
 }
 </script>
 <style type="text/css">
+.editor{
+  display: flex;
+  flex-direction: column;
+}
 .css-output{
    white-space: pre-wrap;
    position: relative;
+   height: 40%;
+   overflow-y: auto;
+   border-top: 1px solid #000;
+}
+.inputs{
+  height: 60%;
+  overflow-y: auto;
 }
 .input{
   border-bottom: 1px solid #000;
   padding:0.5rem;
+}
+.input:last-child{
+  border-bottom: none;
 }
 .input__button{
   display: block;
