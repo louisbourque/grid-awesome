da3cb5c9554a79a2fc0689b7076b2b6a842590e2
Louis Bourque <louis@louisbourque.ca>
2017-09-05 20:41:16 -0600

    Use Vue event dispatching
    Fixes issue where events on newly added areas weren't handled

--- src/components/GridContainer.vue
+++ src/components/GridContainer.vue
@@ -11,6 +11,12 @@
           <grid-item v-for="item, index in areas"
                  :item="item"
                  :key="index"
+                 @dragStart="dragStart"
+                 @dragUpdate="dragUpdate"
+                 @dragEnd="dragEnd"
+                 @resizeStart="resizeStart"
+                 @resizeUpdate="resizeUpdate"
+                 @resizeEnd="resizeEnd"
         >
         </grid-item>
       </template>
@@ -54,6 +60,7 @@ export default {
           y: 0,
         },
       },
+      initialLayout: undefined,
     }
   },
   computed: {
@@ -74,220 +81,200 @@ export default {
       'setDragAreas',
       'addDragArea',
     ]),
-  },
-  mounted() {
-    this.$children.forEach(box => {
-      var initialLayout
-      var isDragging = false
-      var isResizing = false
+    dragStart: function({ item, offset, targetDimensions }) {
+      this.dragging.item = item
+      this.placeholder = utils.cloneItem(this.dragging.item)
+      this.$set(item, 'dragging', true)
+      this.placeholder.label = ''
+      this.placeholder.id = 'placeholder'
+      this.placeholder.w = targetDimensions.x + 'px'
+      this.placeholder.h = targetDimensions.y + 'px'
+      this.placeholder.xCorrection = targetDimensions.x / 2
+      this.placeholder.yCorrection = targetDimensions.y / 2
+      this.placeholder.x = offset.x - this.placeholder.xCorrection
+      this.placeholder.y = offset.y - this.placeholder.yCorrection
 
-      box.$on('dragStart', ({ item, offset, targetDimensions }) => {
-        isDragging = true
+      // clone layout
+      this.initialLayout = utils.cloneLayout(this.areas)
+    },
+    dragUpdate: function(evt) {
+      if (!this.dragging.item) {
+        return
+      }
+      this.dragging.offset = evt.offset
 
-        // find box
-        this.dragging.item = item
-        this.placeholder = utils.cloneItem(this.dragging.item)
-        this.$set(item, 'dragging', true)
-        this.placeholder.label = ''
-        this.placeholder.id = 'placeholder'
-        this.placeholder.w = targetDimensions.x + 'px'
-        this.placeholder.h = targetDimensions.y + 'px'
-        this.placeholder.xCorrection = targetDimensions.x / 2
-        this.placeholder.yCorrection = targetDimensions.y / 2
-        this.placeholder.x = offset.x - this.placeholder.xCorrection
-        this.placeholder.y = offset.y - this.placeholder.yCorrection
+      this.$set(
+        this.placeholder,
+        'x',
+        evt.offset.x - this.placeholder.xCorrection
+      )
+      this.$set(
+        this.placeholder,
+        'y',
+        evt.offset.y - this.placeholder.yCorrection
+      )
 
-        // clone layout
-        initialLayout = utils.cloneLayout(this.areas)
-      })
+      this.setDragAreas([])
+      this.addDragArea(this.dragging.item)
 
-      box.$on('dragUpdate', evt => {
-        if (!isDragging) {
+      let draggingArea = document.querySelector('.dragging')
+
+      if (
+        this.dragging.offset.x >
+        draggingArea.offsetLeft + draggingArea.offsetWidth
+      ) {
+        this.$set(this.dragging.item, 'x', this.dragging.item.x + 1)
+      }
+
+      if (this.dragging.offset.x < draggingArea.offsetLeft) {
+        this.$set(this.dragging.item, 'x', this.dragging.item.x - 1)
+      }
+
+      if (this.dragging.item.x < 0) {
+        this.dragging.item.x = 0
+      }
+      if (this.dragging.item.x >= this.columns.length) {
+        this.dragging.item.x = this.columns.length - 1
+      }
+
+      if (
+        this.dragging.offset.y >
+        draggingArea.offsetTop + draggingArea.offsetHeight
+      ) {
+        this.$set(this.dragging.item, 'y', this.dragging.item.y + 1)
+      }
+
+      if (this.dragging.offset.y < draggingArea.offsetTop) {
+        this.$set(this.dragging.item, 'y', this.dragging.item.y - 1)
+      }
+
+      if (this.dragging.item.y < 0) {
+        this.dragging.item.y = 0
+      }
+      if (this.dragging.item.y >= this.rows.length) {
+        this.dragging.item.y = this.rows.length - 1
+      }
+
+      this.initialLayout.forEach(item => {
+        if (item.id === this.dragging.item.id) {
           return
         }
-        this.dragging.offset = evt.offset
-
-        this.$set(
-          this.placeholder,
-          'x',
-          evt.offset.x - this.placeholder.xCorrection
-        )
-        this.$set(
-          this.placeholder,
-          'y',
-          evt.offset.y - this.placeholder.yCorrection
-        )
-
-        this.setDragAreas([])
-        this.addDragArea(this.dragging.item)
-
-        let draggingArea = document.querySelector('.dragging')
-
-        if (
-          this.dragging.offset.x >
-          draggingArea.offsetLeft + draggingArea.offsetWidth
-        ) {
-          this.$set(this.dragging.item, 'x', this.dragging.item.x + 1)
-        }
-
-        if (this.dragging.offset.x < draggingArea.offsetLeft) {
-          this.$set(this.dragging.item, 'x', this.dragging.item.x - 1)
-        }
-
-        if (this.dragging.item.x < 0) {
-          this.dragging.item.x = 0
-        }
-        if (this.dragging.item.x >= this.columns.length) {
-          this.dragging.item.x = this.columns.length - 1
-        }
-
-        if (
-          this.dragging.offset.y >
-          draggingArea.offsetTop + draggingArea.offsetHeight
-        ) {
-          this.$set(this.dragging.item, 'y', this.dragging.item.y + 1)
-        }
-
-        if (this.dragging.offset.y < draggingArea.offsetTop) {
-          this.$set(this.dragging.item, 'y', this.dragging.item.y - 1)
-        }
-
-        if (this.dragging.item.y < 0) {
-          this.dragging.item.y = 0
-        }
-        if (this.dragging.item.y >= this.rows.length) {
-          this.dragging.item.y = this.rows.length - 1
-        }
-
-        initialLayout.forEach(item => {
-          if (item.id === this.dragging.item.id) {
-            return
-          }
-          this.addDragArea(
-            utils.moveToFreePlace(
-              this.dragAreas,
-              item,
-              this.columns.length - 1,
-              this.rows.length - 1
-            )
+        this.addDragArea(
+          utils.moveToFreePlace(
+            this.dragAreas,
+            item,
+            this.columns.length - 1,
+            this.rows.length - 1
           )
-        })
+        )
       })
+    },
+    dragEnd: function(evt) {
+      if (!this.dragging.item) {
+        return
+      }
+      if (this.dragAreas) {
+        this.setAreas(this.dragAreas)
+      }
 
-      box.$on('dragEnd', evt => {
-        if (!isDragging) {
+      this.$delete(this.dragging.item, 'dragging')
+      this.dragging.item = null
+      this.dragging.offset.x = 0
+      this.dragging.offset.y = 0
+      this.setDragAreas(null)
+    },
+    resizeStart: function({ item, offset }) {
+      this.resizing.item = item
+      this.$set(item, 'resizing', true)
+      this.resizing.offsetStart = offset
+
+      this.initialLayout = utils.cloneLayout(this.areas)
+    },
+    resizeUpdate: function(evt) {
+      if (!this.resizing.item) {
+        return
+      }
+      let previousOffset = this.resizing.offset || { x: 0, y: 0 }
+      this.resizing.offset = evt.offset
+
+      this.setDragAreas([])
+      this.addDragArea(this.resizing.item)
+
+      let changingArea = document.querySelector('.resizing')
+
+      if (
+        previousOffset.x < this.resizing.offset.x &&
+        this.resizing.offset.x + this.resizing.offsetStart.x >
+          changingArea.offsetLeft + changingArea.offsetWidth
+      ) {
+        this.$set(this.resizing.item, 'w', this.resizing.item.w + 1)
+      }
+
+      if (
+        previousOffset.x > this.resizing.offset.x &&
+        this.resizing.offset.x + this.resizing.offsetStart.x <
+          changingArea.offsetLeft + changingArea.offsetWidth
+      ) {
+        this.$set(this.resizing.item, 'w', this.resizing.item.w - 1)
+      }
+
+      if (this.resizing.item.w < 1) {
+        this.resizing.item.w = 1
+      }
+      if (this.resizing.item.w + this.resizing.item.x > this.columns.length) {
+        this.resizing.item.w = this.columns.length - this.resizing.item.x
+      }
+
+      if (
+        previousOffset.y < this.resizing.offset.y &&
+        this.resizing.offset.y + this.resizing.offsetStart.y >
+          changingArea.offsetTop + changingArea.offsetHeight
+      ) {
+        this.$set(this.resizing.item, 'h', this.resizing.item.h + 1)
+      }
+
+      if (
+        previousOffset.y > this.resizing.offset.y &&
+        this.resizing.offset.y + this.resizing.offsetStart.y <
+          changingArea.offsetTop + changingArea.offsetHeight
+      ) {
+        this.$set(this.resizing.item, 'h', this.resizing.item.h - 1)
+      }
+
+      if (this.resizing.item.h < 1) {
+        this.resizing.item.h = 1
+      }
+      if (this.resizing.item.h + this.resizing.item.y > this.rows.length) {
+        this.resizing.item.h = this.rows.length - this.resizing.item.y
+      }
+
+      this.initialLayout.forEach(item => {
+        if (item.id === this.resizing.item.id) {
           return
         }
-        if (this.dragAreas) {
-          this.setAreas(this.dragAreas)
-        }
-
-        this.$delete(this.dragging.item, 'dragging')
-        this.dragging.item = null
-        this.dragging.offset.x = 0
-        this.dragging.offset.y = 0
-        this.setDragAreas(null)
-        isDragging = false
-      })
-
-      box.$on('resizeStart', ({ item, offset }) => {
-        isResizing = true
-
-        this.resizing.item = item
-        this.$set(item, 'resizing', true)
-        this.resizing.offsetStart = offset
-
-        initialLayout = utils.cloneLayout(this.areas)
-      })
-
-      box.$on('resizeUpdate', evt => {
-        if (!isResizing) {
-          return
-        }
-        let previousOffset = this.resizing.offset || { x: 0, y: 0 }
-        this.resizing.offset = evt.offset
-
-        this.setDragAreas([])
-        this.addDragArea(this.resizing.item)
-
-        let changingArea = document.querySelector('.resizing')
-
-        if (
-          previousOffset.x < this.resizing.offset.x &&
-          this.resizing.offset.x + this.resizing.offsetStart.x >
-            changingArea.offsetLeft + changingArea.offsetWidth
-        ) {
-          this.$set(this.resizing.item, 'w', this.resizing.item.w + 1)
-        }
-
-        if (
-          previousOffset.x > this.resizing.offset.x &&
-          this.resizing.offset.x + this.resizing.offsetStart.x <
-            changingArea.offsetLeft + changingArea.offsetWidth
-        ) {
-          this.$set(this.resizing.item, 'w', this.resizing.item.w - 1)
-        }
-
-        if (this.resizing.item.w < 1) {
-          this.resizing.item.w = 1
-        }
-        if (this.resizing.item.w + this.resizing.item.x > this.columns.length) {
-          this.resizing.item.w = this.columns.length - this.resizing.item.x
-        }
-
-        if (
-          previousOffset.y < this.resizing.offset.y &&
-          this.resizing.offset.y + this.resizing.offsetStart.y >
-            changingArea.offsetTop + changingArea.offsetHeight
-        ) {
-          this.$set(this.resizing.item, 'h', this.resizing.item.h + 1)
-        }
-
-        if (
-          previousOffset.y > this.resizing.offset.y &&
-          this.resizing.offset.y + this.resizing.offsetStart.y <
-            changingArea.offsetTop + changingArea.offsetHeight
-        ) {
-          this.$set(this.resizing.item, 'h', this.resizing.item.h - 1)
-        }
-
-        if (this.resizing.item.h < 1) {
-          this.resizing.item.h = 1
-        }
-        if (this.resizing.item.h + this.resizing.item.y > this.rows.length) {
-          this.resizing.item.h = this.rows.length - this.resizing.item.y
-        }
-
-        initialLayout.forEach(item => {
-          if (item.id === this.resizing.item.id) {
-            return
-          }
-          this.addDragArea(
-            utils.moveToFreePlace(
-              this.dragAreas,
-              item,
-              this.columns.length - 1,
-              this.rows.length - 1
-            )
+        this.addDragArea(
+          utils.moveToFreePlace(
+            this.dragAreas,
+            item,
+            this.columns.length - 1,
+            this.rows.length - 1
           )
-        })
+        )
       })
-
-      box.$on('resizeEnd', evt => {
-        if (!isResizing) {
-          return
-        }
-        if (this.dragAreas) {
-          this.setAreas(this.dragAreas)
-        }
-        this.$delete(this.resizing.item, 'resizing')
-        this.resizing.item = null
-        this.resizing.offset.x = 0
-        this.resizing.offset.y = 0
-        this.setDragAreas(null)
-        isResizing = false
-      })
-    })
+    },
+    resizeEnd: function(evt) {
+      if (!this.resizing.item) {
+        return
+      }
+      if (this.dragAreas) {
+        this.setAreas(this.dragAreas)
+      }
+      this.$delete(this.resizing.item, 'resizing')
+      this.resizing.item = null
+      this.resizing.offset.x = 0
+      this.resizing.offset.y = 0
+      this.setDragAreas(null)
+    },
   },
 }
 </script>
